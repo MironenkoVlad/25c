b3e8c7e1cdf81b05608e03469c66b7b08b78b225
artby94 <artembull94@gmail.com>
2026-02-09 22:46:36 +0300

    new AWS config

--- VPC.tf
+++ VPC.tf
@@ -20,10 +20,32 @@ resource "aws_subnet" "subnet1" {
         Name = "Subnet_Class2"
     }
 }
+ resource "aws_internet_gateway" "igw" {
+
+    vpc_id = aws_vpc.class2_VPC.id
+
+    tags = {
+        Name = "IGW_Class2"
+    }
+ }
+
+ resource "aws_route_table" "rt" {
+
+    vpc_id = aws_vpc.class2_VPC.id
+    
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+
+    tags = {
+        Name = "RouteTable_Class2"
+    }
+ }
 
 
+resource "aws_route_table_association" "subnet_assoc" {
 
-
-
-
-
+    subnet_id = aws_subnet.subnet1.id
+    route_table_id = aws_route_table.rt.id
+}
